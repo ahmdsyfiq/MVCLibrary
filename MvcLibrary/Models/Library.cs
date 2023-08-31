f790de5a2482c348f9d14527c72ba35ff828a99e
unknown <ahmdsyfiq@gmail.com>
2023-08-31 09:57:43 +0700

    first

---
+++ MvcLibrary/Models/Library.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcLibrary.Models;
+
+public class Library
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public string? Publisher { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    public decimal Price { get; set; }
+}
